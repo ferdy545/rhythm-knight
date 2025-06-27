d274fd714bc818eae8e25042b9d9cafc86bb2cf6
ferdy545 <fernando.ono2005@gmail.com>
2025-06-27 10:20:36 -0300

    Add beat manager

--- platformer-de-ritmo/test_combat.gd
+++ platformer-de-ritmo/test_combat.gd
@@ -9,6 +9,7 @@ extends Node2D
 @onready var song_beat : AnimationPlayer = $SongBeat
 @onready var rhythms : AnimationPlayer = $Rhythms
 @onready var signal_bus_sender : SignalBusSender = $SignalBusSender
+@onready var signal_bus_receiver : SignalBusReceiver = $SignalBusReceiver
 @onready var player_sprite : Sprite2D = $CharacterBody2D/Sprite2D
 
 @export var _beat = false
@@ -39,20 +40,6 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	$Camera/TestLifeLabel.text = str(player.life)
-		
-	# Start music a bit earlier to sync it with the rhythms (music is 24s long)
-	if not music.playing:
-		music.play(23.9)
-		
-	# Keep the beat "animation" running
-	if not song_beat.is_playing():
-		song_beat.play("song_beat")
-	
-	# Wait for beat to show the sequence
-	if _beat:
-		_beat = false
-		if in_show_sequence and not rhythms.is_playing():
-			rhythms.play(rhythm[0]) # rhythm[0] contains the rhythm itself
 
 	if player.life > 0:
 		show_sequence()
@@ -214,3 +201,8 @@ func enemy_attack():
 	
 func enemy_killed():
 	signal_bus_sender.send_enemy_was_killed(enemy)
+
+
+func _on_signal_bus_receiver_beat_changed() -> void:
+	if in_show_sequence and not rhythms.is_playing():
+		rhythms.play(rhythm[0]) # rhythm[0] contains the rhythm itself
