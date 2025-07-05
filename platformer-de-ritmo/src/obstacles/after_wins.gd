0b9b912f9e384cbccb0f4a7761fbda747af004db
ferdy545 <fernando.ono2005@gmail.com>
2025-07-05 06:34:17 -0300

    Bug fix and change to after-win scene
    - (second attempt) Fixed issue where beat manager would stop registering beats after a certain amount of loops.
    - The button at the scene displayed when completing the level now sends the user to the title scene, instead of replaying the level.

--- platformer-de-ritmo/src/obstacles/after_wins.gd
+++ platformer-de-ritmo/src/obstacles/after_wins.gd
@@ -3,4 +3,6 @@ extends Control
 @export var level_scene : PackedScene
 func _on_button_pressed() -> void:
 	get_tree().paused = false 
-	SceneManager.go_to_level()
+	var title_scene = load("res://title_screen.tscn")
+	var title = title_scene.instantiate()
+	SceneManager._change_to_scene(title)
--- platformer-de-ritmo/src/soundtrack/beat_manager.gd
+++ platformer-de-ritmo/src/soundtrack/beat_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var interval
 var beat = 0
 var current_beat = 0
-@export var bpm := 80
+@export var bpm := 80.0
 @export var _beat = false
 @onready var music = $Music
 @onready var song_beat : AnimationPlayer = $SongBeat
@@ -14,16 +14,18 @@ signal beat_changed
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	music.play()
-	interval = (60.0/bpm) * 2
+	interval = (60.0/bpm) * 2 # In this case, interval = 1.5
 
 
 func _process(delta: float) -> void:
-	if beat >= 16:
+	beat = music.get_playback_position()
+	
+	# For this inequality, any value between 'interval' and 24.0 (playback length) and
+	# reasonably distant from those edge values will work perfectly.
+	if abs(current_beat - beat) >= 15.0: 
 		current_beat = 0
-		
-	beat = floori(music.get_playback_position()/interval)
 	
 	if beat > current_beat:
-		print("BEAT")
-		current_beat = beat
+		current_beat += interval
 		signal_bus_sender.send_beat_changed()
+		print("BEAT")
